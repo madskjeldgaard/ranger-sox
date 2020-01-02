96b8573da571922b07140492783f90481747415b
madskjeldgaard <mail@madskjeldgaard.dk>
2020-01-02 18:00:41 +0100

    first com mit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ranger-sox
-SoX integration for the Ranger File Manager making audio conversions easy
+SoX integration plugin for the [Ranger File Manager](https://github.com/ranger/ranger) making audio conversion easy
+
+# Installation
+Copy `ranger-sox.py` file to ~/.config/ranger/plugins folder and restart ranger.
+
+# Usage
+Select one or more files you want to manipulate using SoX and then execute one of the following commands
+
+Currently available commands:
+- `:norm <filename>` - normalize file to -0.1 db
+- `:trim <filename>` - trim silence off the beginning and end of file
+- `:splitbysilence <filename>` - split a file name into smaller pieces based on silence parts of file
+- `:stereo2mono <filename>` - convert stereo to mono
+- `:fade <filename>` - add small fades to beginning and end of file to avoid clicks
+- `:reverse <filename>` - reverse file
---
+++ ranger-sox.py
@@ -0,0 +1,189 @@
+from __future__ import (absolute_import, division, print_function)
+
+import os
+import subprocess
+
+from ranger.api.commands import Command
+
+
+class sox_base(Command):
+    # The so-called doc-string of the class will be visible in the built-in
+    # help that is accessible by typing "?c" inside ranger.
+    """:sox_base <filename>
+
+    Not really used directly, so don't!
+    """
+
+    # The execute method is called when you run this command in ranger.
+    def execute(self):
+
+        # Do the sox stuff
+        self.perform_on_selected_files()
+
+        # Unmark files when done
+        self.fm.thisdir.mark_all(False)
+
+    def soxcommand(self, fn):
+        # self.fm.notify("Normalizing file " + fn + "!")
+        suffix = "_n"
+
+        newfn = self.filename_with(suffix, fn)
+
+        lvl = (-0.1)
+
+        # The sox operation being used
+        param = f"--norm={lvl}"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+    def filename_with(self, tag, filename):
+        from pathlib import Path
+
+        infile = Path(filename)
+        fn = Path(infile.stem + tag + infile.suffix)
+        fn = infile.parent.joinpath(fn)
+
+        return fn
+
+    def perform_on_selected_files(self):
+        for file in self.fm.thisdir.get_selection():
+            file = file.path
+
+            # Make sure the file exists
+            if not os.path.exists(file):
+                self.fm.notify("The given file does not exist!", bad=True)
+                return
+
+            command = self.soxcommand(file)
+            self.fm.notify("Executing: " + command + "!")
+
+            # execute the command
+            self.fm.execute_command(
+                command,
+                universal_newlines=True,
+                stdout=subprocess.PIPE)
+
+
+class norm(sox_base):
+    """:norm <filename>
+
+    Normalize file to -0.1 db
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_n"
+
+        newfn = self.filename_with(suffix, fn)
+
+        lvl = (-0.1)
+
+        # The sox operation being used
+        param = f"--norm={lvl}"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+
+class trim(sox_base):
+    """:trim <filename>
+
+    Trim silence from beginning and end of file
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_t"
+
+        newfn = self.filename_with(suffix, fn)
+
+        # The sox operation being used
+        param = "silence -l 1 0.125 1% -1 2.0 1% ;"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+
+class splitbysilence(sox_base):
+    """:splitbysilence <filename>
+
+    Split file into several files by detecting silence
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_split"
+
+        newfn = self.filename_with(suffix, fn)
+
+        # The sox operation being used
+        param = "silence 1 0.1 1% 1 0.1 1% : newfile : restart;"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+
+class stereo2mono(sox_base):
+    """:stereo2mono <filename>
+
+    Downmix to mono from stereo
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_mono"
+
+        newfn = self.filename_with(suffix, fn)
+
+        # The sox operation being used
+        param = "remix 1,2 ;"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+
+class fade(sox_base):
+    """:fade <filename>
+
+    Add small fade in/out to file to remove clicks
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_faded"
+
+        newfn = self.filename_with(suffix, fn)
+
+        # The sox operation being used
+        param = "fade t 0.01 0 0.01;"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
+
+
+class reverse(sox_base):
+    """:reverse <filename>
+
+    Reverses audio file
+    """
+
+    def soxcommand(self, fn):
+        suffix = "_rev"
+
+        newfn = self.filename_with(suffix, fn)
+
+        # The sox operation being used
+        param = "reverse;"
+
+        # The full command
+        command = f"sox '{fn}' '{newfn}' {param}"
+
+        return command
